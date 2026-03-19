c1bb3e458a10d1d21b76528328125d7f5aa014d7
Francisco <franciscorosa.work@gmail.com>
2026-03-19 15:54:43 +0000

    Add recursive of the special sequence

---
+++ Special/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Special
+{
+    public class Program
+    {
+        private static int GetSpecial(int n)
+        {
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+
+            return GetSpecial(n - 1) + 2 * GetSpecial(n - 2);
+        }
+        private static void Main(string[] args)
+        {
+            Console.Write("Insert n-th number of the special sequence: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write(GetSpecial(n));
+        }
+    }
+}
